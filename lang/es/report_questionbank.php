d9e93d4bee12f47b208d70ef995ccc41ba02557d
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2025-12-23 05:25:53 -0300

    Add Spanish translation

---
+++ lang/es/report_questionbank.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Cadenas de idioma para el complemento de informe del banco de preguntas.
+ *
+ * @package    report_questionbank
+ * @copyright  2025 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Informe del Banco de Preguntas';
+$string['questionbank:view'] = 'Ver informe del banco de preguntas';
+$string['title'] = 'Informe del Banco de Preguntas';
+$string['questionname'] = 'Nombre de la Pregunta';
+$string['questiontext'] = 'Texto de la Pregunta';
+$string['questiontype'] = 'Tipo de Pregunta';
+$string['category'] = 'Categoría';
+$string['answers'] = 'Respuestas';
+$string['correctanswer'] = 'Respuesta Correcta';
+$string['downloadcsv'] = 'Descargar como CSV';
+$string['noquestions'] = 'No se encontraron preguntas en este curso.';
+$string['filterbycategory'] = 'Filtrar por categoría';
+$string['allcategories'] = 'Todas las categorías';
+$string['privacy:metadata'] = 'El complemento Informe del Banco de Preguntas no almacena ningún dato personal.';
